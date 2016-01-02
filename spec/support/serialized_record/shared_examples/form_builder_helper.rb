f523b13aac786e83072300b90fa2ea3a29b5501c
Krystian Czesak <krystianczesak@gmail.com>
2016-01-01 22:43:35 -0500

    Renaming test domain classes

--- spec/support/serialized_record/shared_examples/form_builder_helper.rb
+++ spec/support/serialized_record/shared_examples/form_builder_helper.rb
@@ -1,24 +1,24 @@
 shared_examples 'SerializedRecord::FormBuilderHelper' do
-  let(:students) { 3.times.collect { double(Student) } }
-  let(:professor) { double(Professor, class: Professor, students: students) }
+  let(:students) { 3.times.collect { double(TestStudent) } }
+  let(:professor) { double(TestProfessor, class: TestProfessor, test_students: students) }
   let(:view_context) { double }
   let(:parent_builder) { double(ActionView::Helpers::FormBuilder, options: { url: 'parent_url' }) }
   subject do
-    TestFormBuilder.new('professor', professor, view_context, url: 'form_url', parent_builder: parent_builder)
+    TestFormBuilder.new('test_professor', professor, view_context, url: 'form_url', parent_builder: parent_builder)
   end
 
   describe '#create_button' do
     before do
       allow(view_context)
         .to receive(:button_tag).with('+',
-                                      name: 'professor[students_attributes][3][_create]',
+                                      name: 'test_professor[test_students_attributes][3][_create]',
                                       value: 1,
-                                      formaction: 'form_url/#students',
+                                      formaction: 'form_url/#test_students',
                                       class: 'btn').and_return('markup')
     end
 
     it 'creates the appropriate markup' do
-      expect(subject.create_button(:students, '+', class: 'btn'))
+      expect(subject.create_button(:test_students, '+', class: 'btn'))
         .to eq('markup')
     end
   end
@@ -27,9 +27,9 @@ shared_examples 'SerializedRecord::FormBuilderHelper' do
     before do
       allow(view_context)
         .to receive(:button_tag).with('-',
-                                      name: 'professor[_destroy]',
+                                      name: 'test_professor[_destroy]',
                                       value: 1,
-                                      formaction: 'parent_url/#professors',
+                                      formaction: 'parent_url/#test_professors',
                                       class: 'btn').and_return('markup')
     end
 
--- spec/support/test_domain.rb
+++ spec/support/test_domain.rb
@@ -1,16 +1,16 @@
-class Student
+class TestStudent
   include ActiveModel::Model
 
   attr_accessor :name
 end
 
-class Professor
+class TestProfessor
   include SerializedRecord::FindOrInitializeFor
   include SerializedRecord::AcceptsNestedAttributeFor
 
-  attr_accessor :students
-  serialized_find_or_initialize_for :students
-  serialized_accepts_nested_attributes_for :students
+  attr_accessor :test_students
+  serialized_find_or_initialize_for :test_students
+  serialized_accepts_nested_attributes_for :test_students
 end
 
 class TestFormBuilder < ActionView::Helpers::FormBuilder
