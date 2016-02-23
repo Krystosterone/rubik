6c2318c7c6b49b53b4e9442fccbbd5c4c0a8b01c
Krystian Czesak <krystianczesak@gmail.com>
2016-02-23 18:26:15 -0500

    Integrating unique jobs with active job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,4 +8,11 @@ elsif Rails.env.production?
   Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
     [user, password] == [ENV.fetch("SIDEKIQ_USERNAME"), ENV.fetch("SIDEKIQ_PASSWORD")]
   end
+
+  Sidekiq.default_worker_options = {
+    unique: :until_executing,
+    unique_args: ->(args) { args.first.except('job_id') }
+  }
+  
+  SidekiqUniqueJobs.config.unique_args_enabled = true
 end
