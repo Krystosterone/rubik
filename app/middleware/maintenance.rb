cfb3a99fcad65a1631327dd917b05e430b57012b
Krystian Czesak <krystianczesak@gmail.com>
2016-02-23 22:34:56 -0500

    Add maintenance page middleware

---
+++ app/middleware/maintenance.rb
@@ -0,0 +1,30 @@
+class Maintenance
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    request = Rack::Request.new(env)
+
+    if show_maintenance_page?(request)
+      [200, {}, [maintenance_page]]
+    else
+      @app.call(env)
+    end
+  end
+
+  private
+
+  def show_maintenance_page?(request)
+    return false unless ENV["MAINTENANCE_MODE"].present?
+    maintainer_ips.exclude?(request.ip)
+  end
+
+  def maintenance_page
+    File.read(Rails.root.join("public/maintenance.html"))
+  end
+
+  def maintainer_ips
+    ENV.fetch("MAINTAINER_IPS", "").split(",")
+  end
+end
