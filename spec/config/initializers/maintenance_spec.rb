fd5bc7b55138ca66c6f4763e449e20c75db84355
Krystian Czesak <krystianczesak@gmail.com>
2016-02-29 22:12:00 -0500

    Adding test on maintenance middleware

---
+++ spec/config/initializers/maintenance_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe Rubik::Application do
+  it "has the Maintenane middleware" do
+    expect(Rails.application.middleware).to include(Maintenance)
+  end
+end
---
+++ spec/middleware/maintenance_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+describe Maintenance do
+  let(:app) { double(Rubik::Application) }
+  subject { described_class.new(app) }
+
+  describe "#call" do
+    context "when maintenance mode is not enabled" do
+      let(:env) { {} }
+
+      it "calls through" do
+        expect(app).to receive(:call).with(env)
+
+        subject.call(env)
+      end
+    end
+
+    context "when maintenance mode is enabled" do
+      before do
+        @maintenance_mode = ENV["MAINTENANCE_MODE"]
+        ENV["MAINTENANCE_MODE"] = "1"
+      end
+      after { ENV["MAINTENANCE_MODE"] = @maintenance_mode }
+
+      context "when the maintainer ips do not include the request ip" do
+        let(:env) { {} }
+        let(:maintenance_page) { File.read(Rails.root.join("public/maintenance.html")) }
+
+        it "shows the maintenance page" do
+          expect(subject.call(env)).to eq([200, {}, [maintenance_page]])
+        end
+      end
+
+      context "when the maintainer ips include the request ip" do
+        let(:env) { { "REMOTE_ADDR" => "123.123.123.123" } }
+        before do
+          @maintainer_ips = ENV["MAINTAINER_IPS"]
+          ENV["MAINTAINER_IPS"] = "123.123.123.123"
+        end
+        after { ENV["MAINTAINER_IPS"] = @maintainer_ips }
+
+        it "calls through" do
+          expect(app).to receive(:call).with(env)
+
+          subject.call(env)
+        end
+      end
+    end
+  end
+end
