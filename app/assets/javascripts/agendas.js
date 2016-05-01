d02bd86616410f2c79418e712f5799876700016a
Krystian Czesak <krystianczesak@gmail.com>
2016-05-01 19:33:27 -0400

    Moving JS

--- app/assets/javascripts/agendas.js
+++ app/assets/javascripts/agendas.js
@@ -1,5 +1,5 @@
-import NestedForm from "agendas/behaviours/nested_form";
-import LeaveNestedForm from "agendas/behaviours/leave_nested_form";
+import NestedForm from "agendas/nested_form";
+import LeaveNestedForm from "agendas/leave_nested_form";
 
 $(() => {
   if (!$(".agendas-controller").length)
