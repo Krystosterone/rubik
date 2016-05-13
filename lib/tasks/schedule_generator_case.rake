949b8bc1fc070a2f6be4f6f926384c867023ba81
Krystian Czesak <krystianczesak@gmail.com>
2016-05-12 21:35:28 -0400

    Adding some Schedule Generator Cases

--- lib/tasks/schedule_generator_case.rake
+++ lib/tasks/schedule_generator_case.rake
@@ -1,6 +1,8 @@
 namespace :schedule_generator_case do
   task write: :environment do
-    schedule_generator_case = ScheduleGeneratorTestCase.new(ENV.fetch("AGENDA_TOKEN"))
-    schedule_generator_case.write
+    ENV.fetch("AGENDA_TOKEN").split(",").each do |token|
+      schedule_generator_case = ScheduleGeneratorTestCase.new(token)
+      schedule_generator_case.write
+    end
   end
 end
--- spec/models/schedule_generator_spec.rb
+++ spec/models/schedule_generator_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 describe ScheduleGenerator do
-  describe '#combine' do
+  describe "#combine" do
     ScheduleGeneratorTestCase.all.each do |schedule_generator_case|
-      context "for agenda #{schedule_generator_case[:token]}" do
+      context "for agenda #{schedule_generator_case[:agenda_attributes][:token]}" do
         let(:expected_course_groups) { schedule_generator_case[:generated_course_groups] }
         let(:actual_course_groups) { agenda.schedules.collect(&:course_groups) }
         let(:agenda) { Agenda.new(schedule_generator_case[:agenda_attributes]) }
