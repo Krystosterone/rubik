67b0828cb3ccede50bd85a555a04f14d00b858d8
Krystian Czesak <krystianczesak@gmail.com>
2016-05-23 18:51:11 -0400

    Removing UQAM code

--- lib/uqam_site.rb
+++
@@ -1,8 +0,0 @@
-require "capybara"
-require "capybara/dsl"
-require "selenium-webdriver"
-
-module UqamSite
-  require "uqam_site/etl"
-  require "uqam_site/etl/download"
-end
--- lib/uqam_site/etl.rb
+++
@@ -1,9 +0,0 @@
-class UqamSite::Etl < Pipeline
-  def initialize(options)
-    super(options)
-  end
-
-  def execute
-    pipe(Download)
-  end
-end
