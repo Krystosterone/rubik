7b8edfa612e6e25c023d88d26bd0ffc213f3b283
Krystian Czesak <krystian@shopify.com>
2017-02-21 20:58:05 -0500

    Getting rid of term decorator and moving delegation from agenda decorator

--- spec/helpers/terms_helper_spec.rb
+++ spec/helpers/terms_helper_spec.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-describe TermDecorator do
-  describe "#title" do
-    subject(:decorator) { described_class.new(term) }
-    let(:term) { instance_double(Term, name: "A name", year: "2015", tags: nil) }
+describe TermsHelper do
+  let(:term) { instance_double(Term, name: "A name", year: "2015", tags: nil) }
 
+  describe "#term_title" do
     context "with a term with no tags" do
       it "returns a concatenation of name year and tags" do
-        expect(decorator.title).to eq("A name 2015")
+        expect(term_title(term)).to eq("A name 2015")
       end
     end
 
@@ -16,7 +15,7 @@ describe TermDecorator do
       before { allow(term).to receive(:tags).and_return("some tags") }
 
       it "returns a concatenation of name year and tags" do
-        expect(decorator.title).to eq("A name 2015 - some tags")
+        expect(term_title(term)).to eq("A name 2015 - some tags")
       end
     end
   end
