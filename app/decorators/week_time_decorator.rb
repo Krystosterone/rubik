45a9eaec507cc564b8c7b7bae5ced20449ff9d35
Krystian Czesak <krystian@shopify.com>
2017-02-22 18:44:10 -0500

    First step in removing WeekdayTimeDecorator

--- app/decorators/week_time_decorator.rb
+++ app/decorators/week_time_decorator.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 class WeekTimeDecorator < WeekdayTimeDecorator
+  include WeekdayTimesHelper
+
   def weekday
     I18n.t("date.day_names")[weekday_index].capitalize
   end
 
   def weekday_time
-    "#{weekday} #{time}"
+    "#{weekday} #{weekday_time_format(self)}"
   end
 end
