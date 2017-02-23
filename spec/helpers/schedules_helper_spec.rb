f5253837c59eaccd5a46309b78ea6824348d3548
Krystian Czesak <krystian@shopify.com>
2017-02-22 22:35:39 -0500

    :boom: last decorator!

--- spec/helpers/schedules_helper_spec.rb
+++ spec/helpers/schedules_helper_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-describe ScheduleDecorator do
-  describe "#css_class" do
-    subject { described_class.new(schedule) }
+describe SchedulesHelper do
+  describe "#schedule_css_class" do
     let(:schedule) do
       instance_double(
         Schedule,
@@ -22,7 +21,9 @@ describe ScheduleDecorator do
       end
       before { allow(schedule).to receive(:weekdays).and_return(weekdays) }
 
-      its(:css_class) { is_expected.to eq("from-600 duration-780 collapsible") }
+      it "returns the correct css class" do
+        expect(helper.schedule_css_class(schedule)).to eq("from-600 duration-780 collapsible")
+      end
     end
 
     context "when not all weekend weekdays are collapsible" do
@@ -35,20 +36,21 @@ describe ScheduleDecorator do
       end
       before { allow(schedule).to receive(:weekdays).and_return(weekdays) }
 
-      its(:css_class) { is_expected.to eq("from-600 duration-780") }
+      it "returns the correct css class" do
+        expect(helper.schedule_css_class(schedule)).to eq("from-600 duration-780")
+      end
     end
   end
 
-  describe "#hours" do
+  describe "#schedule_hours" do
     context "when the starting hour is 12" do
-      subject(:decorator) { described_class.new(schedule) }
       let(:schedule) { instance_double(Schedule, starts_at: instance_double(WeekTime, hour: 12)) }
 
       context "when the end hour is 14" do
         before { allow(schedule).to receive(:ends_at).and_return(instance_double(WeekTime, hour: 14)) }
 
         it "ranges from 12:00 to 14:00" do
-          expect(decorator.hours).to eq(%w(12:00 13:00 14:00))
+          expect(helper.schedule_hours(schedule)).to eq(%w(12:00 13:00 14:00))
         end
       end
 
@@ -56,7 +58,7 @@ describe ScheduleDecorator do
         before { allow(schedule).to receive(:ends_at).and_return(instance_double(WeekTime, hour: 24)) }
 
         it "ranges from 12:00 to 23:00" do
-          expect(decorator.hours).to eq(%w(
+          expect(helper.schedule_hours(schedule)).to eq(%w(
             12:00 13:00 14:00 15:00
             16:00 17:00 18:00 19:00
             20:00 21:00 22:00 23:00
