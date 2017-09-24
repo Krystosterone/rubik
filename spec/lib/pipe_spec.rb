238672496c6cadd37c4f11c325f5943dee6c0d84
Krystian Czesak <krystian@shopify.com>
2017-09-24 01:27:36 -0400

    Simplify spec

--- spec/lib/pipe_spec.rb
+++ spec/lib/pipe_spec.rb
@@ -2,31 +2,11 @@
 require "rails_helper"
 
 describe Pipe do
-  class PipeSpecStep < SimpleClosure
-    def initialize(value)
-      @value = value
-    end
-
-    protected
-
-    attr_reader :value
-  end
-
-  class First < PipeSpecStep
-    def call
-      value + "\nfirst_output"
-    end
-  end
-
-  class Last < PipeSpecStep
-    def call
-      value + "\nlast_output"
-    end
-  end
-
   describe "#call" do
     subject(:pipeline) do
-      described_class.bind(First).bind(Last)
+      described_class
+        .bind(->(value) { value + "\nfirst_output" })
+        .bind(->(value) { value + "\nlast_output" })
     end
     let(:expected_output) do
       <<-OUTPUT.strip_heredoc.strip
