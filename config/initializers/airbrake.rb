e61a486b8f57eabfb4a2649213c111814e95dc6a
Krystian Czesak <krystianczesak@gmail.com>
2022-06-15 15:32:16 -0400

    Updates airbrake config

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 Airbrake.configure do |c|
-  c.project_id = ENV["AIRBRAKE_PROJECT_ID"]
-  c.project_key = ENV["AIRBRAKE_API_KEY"]
+  c.project_id = ENV.fetch("AIRBRAKE_PROJECT_ID")
+  c.project_key = ENV.fetch("AIRBRAKE_API_KEY")
   c.root_directory = Rails.root
-  c.logger = Rails.logger
+  c.logger = Airbrake::Rails.logger
   c.environment = Rails.env
   c.ignore_environments = %w[development test]
+  c.blocklist_keys = [/password/i, /authorization/i]
 end
 
 Airbrake.add_filter do |notice|
