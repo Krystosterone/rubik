97831cd4f8836a5d63ff862fd0770206cf9d9b48
Krystian Czesak <krystianczesak@gmail.com>
2022-06-15 21:48:30 -0400

    Fix schedule pagination

---
+++ spec/systems/schedule_pagination_system_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Schedule Pagination", type: :system do
+  before do
+    driven_by(:rack_test)
+
+    EtsPdf::Etl.call("db/raw/ets/2022/automne/tous/log")
+    Term.first.update!(enabled_at: Time.zone.now)
+  end
+
+  # rubocop:disable RSpec/ExampleLength, RSpec/MultipleExpectations
+  it "pagines schedules correctly" do
+    visit "/"
+    click_on "Génie logiciel"
+
+    expect(page).to have_text("Choix de cours")
+
+    find("label", text: "CHM131").click
+    find("label", text: "LOG121").click
+    within(".courses-per-schedule-group") { find("label", text: "2").click }
+    perform_enqueued_jobs { click_button("Soumettre") }
+
+    expect(page).to have_text("Horaires")
+    expect(page).to have_field("Nombre d'horaires générés", with: "36", disabled: true)
+    within(".pager") do
+      expect(page).to have_text("1")
+      expect(page).to have_text("2")
+      expect(page).to have_text("Suivant ›")
+      expect(page).to have_text("Dernier »")
+
+      click_on "Suivant ›"
+    end
+
+    within(".pager") do
+      expect(page).to have_text("« Premier")
+      expect(page).to have_text("‹ Précédent")
+      expect(page).to have_text("1")
+      expect(page).to have_text("2")
+    end
+  end
+  # rubocop:enable RSpec/ExampleLength, RSpec/MultipleExpectations
+end
