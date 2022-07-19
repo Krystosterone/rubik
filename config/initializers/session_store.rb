67613edd310d46562fd530f5b54beaa1257be4dd
Krystian Czesak <krystianczesak@gmail.com>
2022-07-18 23:39:46 -0400

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,6 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_rubik_session"
+Rails.application.config.session_store :cookie_store,
+                                       domain: "localhost",
+                                       key: "_rubik_session"
