cdf7087a64e5fab8c4aaa7bceed47e3381f55ea6
Krystian Czesak <krystianczesak@gmail.com>
2022-07-21 17:26:41 -0400

    Cleanup JS dependencies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.active_storage.service = :local
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.compile = false
   config.assets.digest = true
   config.log_level = :debug
